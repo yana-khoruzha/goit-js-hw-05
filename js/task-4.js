6a8da3b8e334acc8065df8b14b6fd6cc511189ca
Yana Khoruzha <i.am.kitiket@gmail.com>
2023-12-09 06:07:03 +0200

    add solution

---
+++ js/task-4.js
@@ -0,0 +1,51 @@
+const getTotalBalanceByGender = (users, gender) => {
+  let totalBalance = 0;
+
+  users
+    .filter(user => user.gender === gender)
+    .forEach(user => (totalBalance += user.balance));
+
+  return totalBalance;
+};
+
+const allUsers = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(allUsers, 'male')); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, 'female')); // 8863
